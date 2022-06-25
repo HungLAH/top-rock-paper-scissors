810c7cb3c1c01140003d8d2f32432ee1c89b22e1
Hung Pham <hphamtv@gmail.com>
2022-06-25 17:48:58 +1000

    Refactor play_round() and change indentation space

--- script.js
+++ script.js
@@ -3,60 +3,60 @@ const PAPER_STR = "paper";
 const SCISSORS_STR = "scissors"
 const DEFAULT_PROMPT = "Enter either Rock, Paper or Scissors to begin playing.";
 const ERROR_PROMPT = "Invalid input, please enter only Rock, Paper or Scissors."
+const WIN_OUTCOME = "Win"
+const LOSE_OUTCOME = "Lose"
 
-function getUserSelection() {
-    let userSel = prompt(DEFAULT_PROMPT);
-    while (!isSelectionValid(userSel)) {
-        userSel = prompt(ERROR_PROMPT);
-    }
-    return userSel;
+function getUserSelection(roundNum = 1) {
+  let userSel = prompt(`${DEFAULT_PROMPT} (Round ${roundNum})`);
+  while (!isSelectionValid(userSel)) {
+    userSel = prompt(`${ERROR_PROMPT} (Round ${roundNum}`);
+  }
+  return userSel;
 }
 
 function isSelectionValid(userSel) {
-    return userSel == ROCK_STR || userSel == PAPER_STR || userSel == SCISSORS_STR;
+  if (userSel == undefined) {
+
+  }
+  return userSel == ROCK_STR || userSel == PAPER_STR || userSel == SCISSORS_STR;
 }
 
 function computerPlay() {
-    const randomNum = Math.floor(Math.random() * 3)
-    if (randomNum === 0) {
-        return ROCK_STR;
-    }
-    else if (randomNum == 1) {
-        return PAPER_STR;
-    }
-    else {
-        return SCISSORS_STR;
-    }
+  const randomNum = Math.floor(Math.random() * 3)
+  if (randomNum === 0) {
+    return ROCK_STR;
+  }
+  else if (randomNum == 1) {
+    return PAPER_STR;
+  }
+  else {
+    return SCISSORS_STR;
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log("Player selects " + playerSelection);
-    console.log("Computer selects " + computerSelection);
-    if (playerSelection == computerSelection) {
-        return "It's a draw baby!"
+  console.log(`Player selects ${playerSelection}, Computer selects ${computerSelection}`);
+  if (playerSelection == computerSelection) {
+    return "It's a draw baby!"
+  }
+  else {
+    let playerOutcome;
+    switch (playerSelection) {
+      case ROCK_STR:
+        playerOutcome = (computerSelection == SCISSORS_STR) ? WIN_OUTCOME : LOSE_OUTCOME;
+        break;
+      case PAPER_STR:
+        playerOutcome = (computerSelection == ROCK_STR) ? WIN_OUTCOME : LOSE_OUTCOME;
+        break;
+      case SCISSORS_STR:
+        playerOutcome = (computerSelection == PAPER_STR) ? WIN_OUTCOME : LOSE_OUTCOME;
+        break;
+      default:
+        break;
     }
-    else {
-        switch (playerSelection) {
-            case ROCK_STR:
-                return (    
-                    (computerSelection == SCISSORS_STR) ? `You Won! ${playerSelection} beats ${computerSelection}` : 
-                    `You Lose! ${computerSelection} beats ${playerSelection}`
-                );
-            case PAPER_STR:
-                return (
-                    (computerSelection == ROCK_STR) ? `You Won! ${playerSelection} beats ${computerSelection}` : 
-                    `You Lose! ${computerSelection} beats ${playerSelection}` 
-                )
-            case SCISSORS_STR:
-                return (
-                    (computerSelection == PAPER_STR) ? `You Won! ${playerSelection} beats ${computerSelection}` : 
-                    `You Lose! ${computerSelection} beats ${playerSelection}`
-                )
-            default:
-                return null;
-        }
-    }
-    
+    return playerOutcome == WIN_OUTCOME ? `You ${WIN_OUTCOME}! ${playerSelection} beats ${computerSelection}` :
+      `You ${LOSE_OUTCOME}! ${computerSelection} beats ${playerSelection}`
+  }
 }
 
-console.log(playRound(getUserSelection(), computerPlay()));
+console.log(game());
